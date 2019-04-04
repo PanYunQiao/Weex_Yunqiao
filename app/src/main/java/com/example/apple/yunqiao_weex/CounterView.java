16a3332b51b7aeb78fdb175a604414aa14db7114
skywindvendor_casa <skywind2018casavendor>
2019-04-04 12:20:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/apple/yunqiao_weex/CounterView.java
@@ -0,0 +1,65 @@
+package com.example.apple.yunqiao_weex;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * 姓名    PanJiangHao
+ * 时间    2019/4/4 9:51 AM
+ * 描述    自绘控件，计数器
+ * 测量：onMeasure()决定View的大小；
+ * 布局：onLayout()决定View在ViewGroup(也就是布局)中的位置；
+ * 绘制：onDraw()决定绘制这个View。
+ *
+ * 在CounterView，测量和布局都交了父类，因此只实现了onDraw()方法
+ */
+
+public class CounterView extends View implements View.OnClickListener{
+
+    private Paint mPaint; //准备画笔用于绘制View上的文字
+
+    private Rect mRect; //用于绘制View大小
+
+    private int mCounter;
+
+    /**
+     * 初始化画笔和矩阵
+     * @param context
+     * @param attrs
+     */
+    public CounterView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);//初始化画笔，设置flags:Paint.ANTI_ALIAS_FLAG扛锯齿
+        mRect = new Rect();
+        setOnClickListener(this);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        mPaint.setColor(Color.BLUE);//设置画笔颜色
+        //在画布上画出一个矩阵，上左下右可以自己设置,画布颜色就是上一步画笔的颜色
+        canvas.drawRect(0,0,getWidth(),getHeight(),mPaint);
+        //现在改变画笔颜色，绘制画笔的数,设置字体大小,getTextBounds获得文字的宽高
+        mPaint.setColor(Color.YELLOW);
+        mPaint.setTextSize(30);
+        String text = String.valueOf(mCounter);
+        mPaint.getTextBounds(text,0,text.length(),mRect);
+        float textWidth = mRect.width();
+        float textHeight = mRect.height();
+        canvas.drawText(text,getWidth() / 2 - textWidth / 2, getHeight() / 2
+                + textHeight / 2, mPaint);
+    }
+
+    @Override
+    public void onClick(View v) {
+        mCounter++;
+        invalidate();//调用invalidate()方法会导致视图进行重绘，因此onDraw()方法在稍后就将会得到调用。视图会相应更新
+    }
+}
---
+++ app/src/main/java/com/example/apple/yunqiao_weex/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.apple.yunqiao_weex;
+
+import android.os.Handler;
+import android.os.Message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * 对于MVP的理解及使用
+ */
+interface MVPView{
+    void upDateTv(String text);
+}
+public class MainActivity extends AppCompatActivity implements MVPView{
+    TextView tv;
+    Button btn;
+    Presenter presenter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
+        presenter = new Presenter(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.result();
+            }
+        });
+    }
+
+    @Override
+    public void upDateTv(String text) {
+        tv.setText(text);
+    }
+
+    interface Callback{
+        void onResult(String text);
+    }
+
+    public class HttpModle{
+        Callback callback;
+
+        HttpModle(Callback callback){
+            this.callback = callback;
+        }
+
+        private Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                callback.onResult((String) msg.obj);
+            }
+        };
+        public void result(){
+            new Thread(){
+                @Override
+                public void run() {
+                    try {
+                        Thread.sleep(2000);
+                        Message message = new Message();
+                        message.obj = "从网络获取的数据";
+                        handler.sendMessage(message);
+
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }.start();
+        }
+    }
+
+    public class Presenter{
+        MVPView view;
+        HttpModle modle;
+
+        public Presenter(MVPView view){
+            this.view = view;
+            modle = new HttpModle(new Callback() {
+                @Override
+                public void onResult(String text) {
+                    Presenter.this.view.upDateTv(text);
+                }
+            });
+        }
+
+        public void result(){
+            modle.result();
+        }
+        private void  detachView(){
+            view = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        presenter.detachView();
+        super.onDestroy();
+    }
+}
+
